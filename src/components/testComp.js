c37b6bfd839b9491f0d3c013ec6a1b15b077e04b
Lee Cooper <lee.cooper@drllimited.co.uk>
2017-02-18 18:24:57 +0000

    adding react boilerplate

---
+++ src/components/testComp.js
@@ -0,0 +1,17 @@
+import React, {
+    Component,
+    PropTypes,
+} from 'react';
+
+class testComp extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+testComp.propTypes = {};
+testComp.defaultProps = {};
+
+export default testComp;
